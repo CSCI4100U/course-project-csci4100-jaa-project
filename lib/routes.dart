03bb647f44642aa14274147bd1a4fa52c8df9e1c
joacotome24 <joaco.tome@gmail.com>
2022-11-21 14:16:58 -0500

    created map screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:course_project/screens/event/event_screen.dart';
 import 'package:course_project/screens/event_form/event_form.dart';
 import 'package:course_project/screens/home/components/events_list.dart';
+import 'package:course_project/screens/map/map_screen.dart';
 import 'package:course_project/screens/profile/profile_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:course_project/screens/details/details_screen.dart';
@@ -13,6 +14,7 @@ final Map<String, WidgetBuilder> routes = {
   ProfileScreen.routeName: (context) => ProfileScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
   EventForm.routeName: (context) => EventForm(),
+  MapScreen.routeName: (context) => MapScreen(),
   EventsList.routeName: (context) => EventsList(),
   EventScreen.routeName: (context) => EventScreen(),
 };
---
+++ lib/screens/map/map_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+    static String routeName = "/map_screen";
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
