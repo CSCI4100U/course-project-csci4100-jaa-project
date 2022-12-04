27896cc6e7d64a741b5e028271b3923fb45d5e97
tin <agus.hernandez9@gmail.com>
2022-12-03 21:00:20 -0300

    Add notifications and notifications screen

---
+++ lib/models/db_models/notifications.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart';
+
+class Notifications {
+  final channelId = "Events Application Notifications";
+  final channelName = "Events Application Notifications";
+  final channelDescription =
+      "Channel for notifications of the events application";
+
+  //Configure plugin using platform specific details
+  var _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  NotificationDetails? _platformChannelInfo;
+  var _notificationID = 100;
+
+  // Request permission to show notifications
+  Future init() async {
+    if (Platform.isIOS) {
+      _requestIOSPermission();
+    }
+
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('mipmap/ic_launcher');
+
+    var initializationSettingsIOS = DarwinInitializationSettings(
+      onDidReceiveLocalNotification:
+          (int id, String? title, String? body, String? payload) {
+        return null;
+      },
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse,
+    );
+
+    var androidChannelInfo = AndroidNotificationDetails(channelId, channelName,
+        channelDescription: channelDescription);
+
+    var iosChannelInfo = DarwinNotificationDetails();
+
+    _platformChannelInfo = NotificationDetails(
+      android: androidChannelInfo,
+      iOS: iosChannelInfo,
+    );
+  }
+
+  void sendNotificationNow(String title, String body, String payload) {
+    print(_flutterLocalNotificationsPlugin.toString());
+    _flutterLocalNotificationsPlugin.show(
+        _notificationID++, title, body, _platformChannelInfo,
+        payload: payload);
+  }
+
+  Future sendNotificationLater(
+      String title, String body, String payload, TZDateTime when) {
+    return _flutterLocalNotificationsPlugin.zonedSchedule(
+      _notificationID++,
+      title,
+      body,
+      when,
+      _platformChannelInfo!,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      payload: payload,
+    );
+  }
+
+  Future<List<PendingNotificationRequest>>
+      getPendingNotificationRequests() async {
+    return _flutterLocalNotificationsPlugin.pendingNotificationRequests();
+  }
+
+  Future onDidReceiveNotificationResponse(
+      NotificationResponse notificationResponse) async {
+    if (notificationResponse != null) {
+      print("NotificationResponse::payload = "
+          "${notificationResponse.payload}");
+    }
+  }
+
+  Future<int> getNotificationsCount() async {
+    return (await getPendingNotificationRequests()).length;
+  }
+
+  _requestIOSPermission() {
+    _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()!
+        .requestPermissions(
+          sound: true,
+          badge: true,
+          alert: true,
+        );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/widgets.dart';
+
 import 'package:course_project/screens/event/event_screen.dart';
 import 'package:course_project/screens/event_form/event_form.dart';
 import 'package:course_project/screens/event_form/location_map.dart';
 import 'package:course_project/screens/home/components/events_list.dart';
 import 'package:course_project/screens/map/map_screen.dart';
 import 'package:course_project/screens/profile/profile_screen.dart';
-import 'package:flutter/widgets.dart';
 import 'package:course_project/screens/details/details_screen.dart';
 import 'package:course_project/screens/home/home_screen.dart';
+import 'package:course_project/screens/notifications/notifications_screen.dart';
 
 // We use name route
 // All our routes will be available here
@@ -19,4 +21,5 @@ final Map<String, WidgetBuilder> routes = {
   EventsList.routeName: (context) => EventsList(),
   EventScreen.routeName: (context) => EventScreen(),
   LocationMap.routeName: (context) => LocationMap(),
+  NotificationsScreen.routeName: (context) => NotificationsScreen(),
 };
