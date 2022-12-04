887ff94a728999522f53a0d1fe515f60fb8a457c
anthony9105 <anthony.liscio@ontariotechu.net>
2022-12-03 21:09:25 -0500

    added EventTable route

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,7 @@ import 'package:course_project/screens/event_form/location_map.dart';
 import 'package:course_project/screens/home/components/events_list.dart';
 import 'package:course_project/screens/map/map_screen.dart';
 import 'package:course_project/screens/profile/profile_screen.dart';
+import 'package:course_project/screens/table/event_table.dart';
 import 'package:flutter/widgets.dart';
 import 'package:course_project/screens/details/details_screen.dart';
 import 'package:course_project/screens/home/home_screen.dart';
@@ -19,4 +20,5 @@ final Map<String, WidgetBuilder> routes = {
   EventsList.routeName: (context) => EventsList(),
   EventScreen.routeName: (context) => EventScreen(),
   LocationMap.routeName: (context) => LocationMap(),
+  EventTable.routeName: (context) => EventTable(),
 };
