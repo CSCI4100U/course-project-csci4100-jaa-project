1286cfce6fb627cfa7123aafc15f906473434d16
anthony9105 <anthony.liscio@ontariotechu.net>
2022-12-05 14:22:29 -0500

    added a setstate so the calendar events show up right away and added more comments

--- lib/screens/calendar/calendar_view.dart
+++ lib/screens/calendar/calendar_view.dart
@@ -4,7 +4,7 @@ import 'package:course_project/models/db_models/event_model.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 
-
+/// Calendar View Screen
 class CalendarView extends StatefulWidget {
   const CalendarView({Key? key}) : super(key: key);
   static String routeName = "/calendarView";
@@ -15,8 +15,10 @@ class CalendarView extends StatefulWidget {
 
 class _CalendarViewState extends State<CalendarView> {
   late CalendarController _controller;
-  EventModel _eventModel = EventModel();
+  final EventModel _eventModel = EventModel();
+
   List<dynamic> selectedEvents = [];
+  // Map of DateTime keys, and values of Lists of events
   final eventDateTimes = Map<DateTime, List<dynamic>>();
 
   @override
@@ -29,7 +31,7 @@ class _CalendarViewState extends State<CalendarView> {
   }
 
 
-
+  /// from the cloud database, get all the events for each DateTime
   void getAllEventsDateTimes () async {
     var events = await _eventModel.getAllEvents();
     for (var event in events) {
@@ -37,9 +39,16 @@ class _CalendarViewState extends State<CalendarView> {
       var eventDate = DateTime(eventDateTime.year, eventDateTime.month, eventDateTime.day);
       if (eventDateTimes.containsKey(eventDate)) {
         eventDateTimes[eventDate]!.add(event);
-      } else {
+      }
+      else {
         eventDateTimes[eventDate] = [event];
       }
+
+      // just using a set state so that the calendar has the events added to
+      // it right away
+      setState(() {
+        print("Calendar started");
+      });
     }
   }
   @override
@@ -56,31 +65,43 @@ class _CalendarViewState extends State<CalendarView> {
               rowHeight: 70,
               events: eventDateTimes,
               initialCalendarFormat: CalendarFormat.month,
+
+              /// set weekendDays to none because I don't want these days to
+              /// have a different colour unless they have events on them
               weekendDays: [],
+
               calendarStyle: CalendarStyle(
                   markersMaxAmount: 3,
                   canEventMarkersOverflow: true,
+
+                  /// text style for days with event(s) on them
                   eventDayStyle: const TextStyle(
                       color: Colors.red,
                       fontWeight: FontWeight.bold,
                   ),
-                  todayColor: Colors.green,
+                  todayColor: Colors.blue,
                   selectedColor: Theme.of(context).primaryColor,
+
+                  /// text style for a selected day
                   todayStyle: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30.0,
                       color: Colors.white)
               ),
+
+              /// header style (where it says "month", "2 weeks", or "week")
               headerStyle: HeaderStyle(
                 centerHeaderTitle: true,
                 formatButtonDecoration: BoxDecoration(
                   color: Colors.blueGrey,
                   borderRadius: BorderRadius.circular(30.0),
                 ),
-                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonTextStyle: const TextStyle(color: Colors.white),
                 formatButtonShowsNext: false,
               ),
               startingDayOfWeek: StartingDayOfWeek.sunday,
+
+              /// get the current selectedEvents and store into the list
               onDaySelected: 
                   (date, events, _) {
                   setState(() {
@@ -100,6 +121,8 @@ class _CalendarViewState extends State<CalendarView> {
                     style: const TextStyle(color: Colors.white),
                   ),
                 ),
+
+                /// blue box for selected day
                 todayDayBuilder: (context, date, events) => Container(
                   margin: const EdgeInsets.all(5.0),
                   alignment: Alignment.center,
@@ -115,6 +138,8 @@ class _CalendarViewState extends State<CalendarView> {
               ),
               calendarController: _controller,
             ),
+
+            /// show all the events (if any) at the bottom, for the selected day
             ...selectedEvents.map((event) => ListTile(
                 title: Text(convertDateTimeToDate(event.date)),
                 subtitle: Text(event.name),
@@ -135,10 +160,5 @@ class _CalendarViewState extends State<CalendarView> {
   String convertDateTimeToTime (DateTime date) {
     final dateFormat = DateFormat('Hm');
     return dateFormat.format(date);
-  }
-
-  DateTime simplifyDateTime (DateTime date) {
-    final dateFormat = DateFormat('y-M-d');
-    return DateTime.parse(dateFormat.format(date));
   }
 }
