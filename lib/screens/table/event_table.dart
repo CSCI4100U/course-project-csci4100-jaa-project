23257486f42bb9b76267760b19644949ed3c787e
anthony9105 <anthony.liscio@ontariotechu.net>
2022-12-04 20:11:25 -0500

    created calendar_view

--- lib/screens/table/event_table.dart
+++ lib/screens/table/event_table.dart
@@ -13,10 +13,16 @@ class EventTable extends StatefulWidget {
 }
 
 class _EventTableState extends State<EventTable> {
-  final List<String> columnNames = ['id', 'Name', 'Price', 'Rating', 'Date', 'Date Created'];
+  final List<String> columnNames = ['id', 'Name', 'Price', 'Rating', 'Capacity',
+    'Rating per Price', 'Capacity per Price','Date', 'Date Created'
+  ];
   final EventModel _eventModel = EventModel();
   int currentId= 0;
   late List<Map> dataItems = List.empty(growable: true);
+  final TextStyle tableTitleTextStyle = const TextStyle(
+    color: Colors.indigo,
+    fontSize: 25,
+  );
 
   @override
   void initState() {
@@ -42,38 +48,84 @@ class _EventTableState extends State<EventTable> {
         /// so the user can scroll through the datatable items vertically
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: DataTable(
-            columnSpacing: 10,
-            sortAscending: isAscending,
-            sortColumnIndex: currentSortCol,
-            columns: [
-              // NAME COLUMN
-              sortedColumnWidget(columnNames[1], columnNames[1]),
-              // PRICE COLUMN
-              sortedColumnWidget(columnNames[2], columnNames[2]),
-              // RATING COLUMN
-              sortedColumnWidget(columnNames[3], columnNames[3]),
-              // DATE COLUMN
-              sortedColumnWidget(columnNames[4], columnNames[4]),
-              // CREATED AT DATE COLUMN
-              sortedColumnWidget(columnNames[5], columnNames[5]),
+          child: Row (
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// downward arrow (to show user they can scroll up and down)
+              const Padding(
+                padding: EdgeInsets.only(top: 60.0, left: 10),
+                child: Icon(Icons.arrow_circle_down, size: 50, color: Colors.indigo,),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /// data table title
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Events Data Table    ",
+                          style: tableTitleTextStyle,
+                        ),
+                        /// right arrow to show the user they can scroll right
+                        /// and left)
+                        const Icon(
+                          Icons.arrow_circle_right_outlined,
+                          size: 50,
+                          color: Colors.indigo,
+                        ),
+                      ],
+                    ),
+                  ),
+                  /// data table
+                  DataTable(
+                    columnSpacing: 10,
+                    sortAscending: isAscending,
+                    sortColumnIndex: currentSortCol,
+                    columns: [
+                      // NAME COLUMN
+                      sortedColumnWidget(columnNames[1], columnNames[1]),
+                      // PRICE COLUMN
+                      sortedColumnWidget(columnNames[2], columnNames[2]),
+                      // RATING COLUMN
+                      sortedColumnWidget(columnNames[3], columnNames[3]),
+                      // CAPACITY COLUMN
+                      sortedColumnWidget(columnNames[4], columnNames[4]),
+                      // RATING PER PRICE COLUMN
+                      sortedColumnWidget(columnNames[5], columnNames[5]),
+                      // CAPACITY PER PRICE COLUMN
+                      sortedColumnWidget(columnNames[6], columnNames[6]),
+                      // DATE COLUMN
+                      sortedColumnWidget(columnNames[7], columnNames[7]),
+                      // CREATED AT DATE COLUMN
+                      sortedColumnWidget(columnNames[8], columnNames[8]),
+                    ],
+                    rows: dataItems.map((item){
+                      return generateData(
+                        item[columnNames[1]],
+                        item[columnNames[2]],
+                        item[columnNames[3]],
+                        item[columnNames[4]],
+                        item[columnNames[5]],
+                        item[columnNames[6]],
+                        item[columnNames[7]],
+                        item[columnNames[8]],
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ],
-            rows: dataItems.map((item){
-              return generateData(
-                item[columnNames[1]],
-                item[columnNames[2]],
-                item[columnNames[3]],
-                item[columnNames[4]],
-                item[columnNames[5]],
-              );
-            }).toList(),
           ),
         ),
       ),
     );
   }
 
-  DataRow generateData(String? name, double? price, double? rating, DateTime date, DateTime createdDate){
+  DataRow generateData(String? name, double? price, double? rating, int? capacity,
+      double? ratingPerPrice, double? capacityPerPrice, DateTime date,
+      DateTime createdDate){
     return DataRow(
         cells: [
           /// name
@@ -82,10 +134,16 @@ class _EventTableState extends State<EventTable> {
           DataCell(Text(price.toString()),),
           /// rating
           DataCell(Text(rating.toString()),),
+          /// capacity
+          DataCell(Text(capacity.toString())),
+          /// rating per price
+          DataCell(Text(ratingPerPrice!.toStringAsFixed(2)),),
+          /// capacity per price
+          DataCell(Text(capacityPerPrice!.toStringAsFixed(2)),),
           /// date
-          DataCell(Text(condeseDateTime(date))),
+          DataCell(Text(condenseDateTime(date))),
           /// date created
-          DataCell(Text(condeseDateTime(createdDate))),
+          DataCell(Text(condenseDateTime(createdDate))),
         ]
     );
   }
@@ -127,8 +185,11 @@ class _EventTableState extends State<EventTable> {
       newDataItem[columnNames[1]] = events[i].name;
       newDataItem[columnNames[2]] = events[i].price;
       newDataItem[columnNames[3]] = events[i].rating;
-      newDataItem[columnNames[4]] = events[i].date;
-      newDataItem[columnNames[5]] = events[i].createdAt;
+      newDataItem[columnNames[4]] = events[i].capacity;
+      newDataItem[columnNames[5]] = events[i].rating / events[i].price!;
+      newDataItem[columnNames[6]] = events[i].capacity!.toDouble() / events[i].price!;
+      newDataItem[columnNames[7]] = events[i].date;
+      newDataItem[columnNames[8]] = events[i].createdAt;
 
       setState(() {
         dataItems.add(newDataItem);
@@ -140,7 +201,7 @@ class _EventTableState extends State<EventTable> {
   /// change DateTime to a string in just YYYY-MM-DD format.  (Used for better
   /// viewing for the user, because the user does not need to see the minutes,
   /// seconds, etc)
-  String condeseDateTime (DateTime date) {
+  String condenseDateTime (DateTime date) {
     return "${date.year}-${date.month}-${date.day}";
   }
 }
